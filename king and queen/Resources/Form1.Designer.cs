e4cd80d00e47a7a5021f9ee575efb8fedb15cb5c
AbhishekPeddi55 <peddiabhishek55@gmail.com>
2025-02-08 18:33:39 -0500

    Add a linke Button to the game

--- king and queen/Resources/Form1.Designer.cs
+++ king and queen/Resources/Form1.Designer.cs
@@ -5,20 +5,21 @@
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.Button btnStartGame;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.LinkLabel linkLabel1;
 
         private void InitializeComponent()
         {
             btnStartGame = new Button();
             btnExit = new Button();
-            button1 = new Button();
+            linkLabel1 = new LinkLabel();
             SuspendLayout();
             // 
             // btnStartGame
             // 
             btnStartGame.BackColor = Color.FromArgb(255, 192, 128);
-            btnStartGame.Location = new Point(100, 70);
+            btnStartGame.Location = new Point(100, 64);
             btnStartGame.Name = "btnStartGame";
-            btnStartGame.Size = new Size(109, 26);
+            btnStartGame.Size = new Size(109, 32);
             btnStartGame.TabIndex = 0;
             btnStartGame.Text = "Start Game";
             btnStartGame.UseVisualStyleBackColor = false;
@@ -27,29 +28,33 @@
             // btnExit
             // 
             btnExit.BackColor = Color.FromArgb(255, 192, 128);
-            btnExit.Location = new Point(100, 129);
+            btnExit.Location = new Point(100, 133);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(109, 29);
+            btnExit.Size = new Size(109, 28);
             btnExit.TabIndex = 1;
             btnExit.Text = "Exit Application";
             btnExit.UseVisualStyleBackColor = false;
             btnExit.Click += btnExit_Click;
             // 
-            // button1
+            // linkLabel1
             // 
-            button1.BackColor = Color.FromArgb(255, 192, 128);
-            button1.Location = new Point(100, 102);
-            button1.Name = "button1";
-            button1.Size = new Size(108, 23);
-            button1.TabIndex = 2;
-            button1.Text = "Rules";
-            button1.UseVisualStyleBackColor = false;
+            linkLabel1.BackColor = Color.FromArgb(255, 192, 128);
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(100, 99);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(109, 31);
+            linkLabel1.TabIndex = 2;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Game Rules";
+            linkLabel1.TextAlign = ContentAlignment.MiddleCenter;
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked_1;
             // 
             // Form1
             // 
+            BackColor = SystemColors.ActiveCaptionText;
             BackgroundImage = king_and_queen.Properties.Resources.OIP;
-            ClientSize = new Size(339, 181);
-            Controls.Add(button1);
+            ClientSize = new Size(344, 180);
+            Controls.Add(linkLabel1);
             Controls.Add(btnExit);
             Controls.Add(btnStartGame);
             Name = "Form1";
@@ -57,7 +62,5 @@
             Load += Form1_Load;
             ResumeLayout(false);
         }
-
-        private Button button1;
     }
 }
--- king and queen/Resources/Form1.cs
+++ king and queen/Resources/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 using king_and_queen.Resources;
 
@@ -19,6 +20,23 @@ namespace TicTacToeApp
             this.Hide();
         }
 
+        // Open URL in Default Browser
+        private void openUrl(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true,
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening link: " + ex.Message);
+            }
+        } // <-- Fixed missing closing bracket here
+
         // Button Click: Exit Application
         private void btnExit_Click(object sender, EventArgs e)
         {
@@ -27,7 +45,18 @@ namespace TicTacToeApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Code to execute when Form1 loads (if needed)
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Add button1 click functionality if needed
+        }
+
+        // LinkLabel Click Event
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            openUrl("https://www.gvsu.edu/cms4/asset/AF5F8044-C1CA-D385-2E8007C14930B056/not_your_normal_tic_tac_toe_game_rules.pdf");
         }
     }
 }
